7f5e58f3fbf6bbbcbddc6ee2a60cf5728607e857
Joe Hawes <morehawes@gmail.com>
2026-04-01 12:09:22 -0700

    Revert "feature: Recording"
    This reverts commit 6366aedeac06ad7bf16cc1ede4f0c4d0d1e43242.

--- src/main.js
+++ src/main.js
@@ -2,11 +2,8 @@ import "maplibre-gl/dist/maplibre-gl.css"; // MapLibre GL JS — required for th
 import Navigator from "@ogis/navigator";
 import "./theme.scss";
 
-import { RecordingsPlugin } from "./recordings/recordings.js";
-
 const nav = Navigator.create({
   id: "app",
-  plugins: [RecordingsPlugin],
   debug: false, // Optional: Enable debug mode
   messages: {
     // English (default)

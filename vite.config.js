9b988dfdc680dcb278d6999036662bc9d8779bae
Joe Hawes <morehawes@gmail.com>
2026-04-02 11:53:14 -0700

    Base & Bump

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 
 export default defineConfig({
-  base: "/onrte-app/",
+  base: "/",
   plugins: [vue()],
   test: {
     environment: "jsdom",
